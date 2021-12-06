a7da993d4bcfe44fb93aca48f5ce6853bf0e53f4
baiyongjian <yongjian.bai@nriat.com>
2021-12-06 19:08:51 +0800

    feat: '更新 stylelint 配置'

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,16 +1,18 @@
+// https://stylelint.io/migration-guide/to-14
 module.exports = {
   extends: [
     // Use the Standard config as the base
     // https://github.com/stylelint/stylelint-config-standard
     'stylelint-config-standard',
+    // https://github.com/ota-meshi/stylelint-config-html#readme
+    'stylelint-config-html',
     // Enforce a standard order for CSS properties
     // https://github.com/stormwarning/stylelint-config-recess-order
-    'stylelint-config-recess-order'
-    // Override rules that would interfere with Prettier
-    // https://github.com/shannonmoeller/stylelint-config-prettier
-  ],
-  plugins: [
-    'stylelint-scss'
+    'stylelint-config-recess-order',
+    // https://www.npmjs.com/package/stylelint-config-standard-scss
+    'stylelint-config-standard-scss',
+    // https://www.npmjs.com/package/stylelint-config-recommended-vue
+    'stylelint-config-recommended-vue'
   ],
   rules: {
     'comment-empty-line-before': [
@@ -28,19 +30,10 @@ module.exports = {
     'selector-max-universal': 1,
     // Disallow allow global element/type selectors in scoped modules
     'selector-max-type': [0, { ignore: ['child', 'descendant', 'compounded'] }],
-    // ===
+    // BEM 规则
+    'selector-class-pattern': '^([a-z][a-z0-9]*)|(--|-|__|\.[a-z0-9]+).*$',
+    'string-quotes': 'single',
     // SCSS
-    // ===
-    'scss/dollar-variable-colon-space-after': 'always',
-    'scss/dollar-variable-colon-space-before': 'never',
-    'scss/dollar-variable-no-missing-interpolation': true,
-    'scss/dollar-variable-pattern': [/^[a-z-]+$/],
-    'scss/double-slash-comment-whitespace-inside': 'always',
-    'scss/operator-no-newline-before': true,
-    'scss/operator-no-unspaced': true,
-    'scss/selector-no-redundant-nesting-selector': true,
-    // Allow SCSS and CSS module keywords beginning with `@`
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
+    'scss/at-import-partial-extension': 'always'
   }
 }
